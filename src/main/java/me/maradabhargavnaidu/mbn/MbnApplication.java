82b0c40255d33df57bd689aaf6645edd60cf711c
maradabhargavnaidu <bhargav.naidu.mbn.18@gmail.com>
2025-09-06 06:18:16 +0530

    maven -spring

---
+++ src/main/java/me/maradabhargavnaidu/mbn/MbnApplication.java
@@ -0,0 +1,20 @@
+package me.maradabhargavnaidu.mbn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@SpringBootApplication
+public class MbnApplication {
+	@GetMapping("/")
+	public String Hello() {
+		return "Hello from Maven MBN";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbnApplication.class, args);
+	}
+
+}
