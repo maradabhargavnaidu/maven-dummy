1d49f8814f965594273f9a024f078a6ace287450
MBN <87939515+maradabhargavnaidu@users.noreply.github.com>
2026-05-13 19:27:49 +0530

    Update MbnApplication.java

--- src/main/java/me/maradabhargavnaidu/mbn/MbnApplication.java
+++ src/main/java/me/maradabhargavnaidu/mbn/MbnApplication.java
@@ -2,22 +2,11 @@ package me.maradabhargavnaidu.mbn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
 @SpringBootApplication
-public class MbnApplication extends SpringBootServletInitializer{
-	@GetMapping("/")
-	public String Hello() {
-		return "Hello from Maven MBN";
-	}
-	@override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
-		return builder.soruces(MbnApplication.class);
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(MbnApplication.class, args);
-	}
+public class MbnApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(MbnApplication.class, args);
+    }
 }
